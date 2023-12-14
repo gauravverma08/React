c9cec44a0cb3824f648bbe94f339657224610df8
Gaurav Verma <gaurav22vrm@gmail.com>
2023-12-14 06:09:08 +0530

    adding

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 // import Accordian from './Components/Accordian';
 // import Navbarr from './Components/Navbarr';
-import Textapp from './Components/Textapp';
+// import Textapp from './Components/Textapp';
+// import Home from "./Components/Home.jsx";
+import Nav from "./Components/Navigation.jsx";
 
 function App() {
   return (
     <>
-    <h1>this is a heading</h1>
+    <div>
+      <h1>Star Store</h1>
+      <Nav name ={"Home"}/>
+      <Nav name ={"Blogs"}/>
+      <Nav name ={"Contacts"}/>
+    </div>
     </>
   );
 }
---
+++ myfirstapp/src/Components/Navigation.jsx
@@ -0,0 +1,8 @@
+const Nav = ({name})=>{
+    //props
+    return(
+        <h1>{name}</h1>
+    )
+}
+
+export default Nav;
